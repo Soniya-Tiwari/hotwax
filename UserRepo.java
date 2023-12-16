fcb3a43481aec909375aa60f1bef0835d026211b
Soniya Tiwari <113426704+Soniya-Tiwari@users.noreply.github.com>
2023-12-16 18:05:16 +0530

    Add files via upload

--- UserRepo.java
+++ UserRepo.java
@@ -1,4 +1,9 @@
-package com.Assignment.app.rest;
+package com.Assignment.app.rest.Repo;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo {
+public interface UserRepo extends JpaRepository<User, Long> {
+
+
+
 }
